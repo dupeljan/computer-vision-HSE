557b9c3b6361c472498021590f48c04eeb981c00
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-01-07 22:38:10 +0300

    Add more effects

--- shift_effect.py
+++ shift_effect.py
@@ -5,11 +5,14 @@ def main():
     # define a video capture object
     vid = cv2.VideoCapture(0)
     th, tw = 0, 0
-    speedh, speedw = 10, 10
+    speedh, speedw = 6, 6
     w, h = vid.read()[1].shape[:-1]
+    out = cv2.VideoWriter('outpy_1.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 30, (h, w))
+
+
     while(True):
-        th = th + speedh if th < h else 0
-        tw = tw + speedw if tw < w else 0
+        th = th + speedh if th + speedh < h else 0
+        tw = tw + speedw if tw + speedw < w else 0
 
         shifth = int(th)
         shiftw = int(tw)
@@ -21,7 +24,12 @@ def main():
                                                     frame[:, shifth:, :], frame[:, :shifth, :]
         new_frame[:(w - shiftw), :, :], new_frame[(w - shiftw):, :, :] = \
             new_frame[shiftw:, :, :], new_frame[:shiftw, :, :].copy()
+        # Coloring
+        new_frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
+        new_frame = cv2.threshold(new_frame, 127, 255, cv2.THRESH_BINARY)[1]
         # Display the resulting frame
+        new_frame = cv2.cvtColor(new_frame, cv2.COLOR_GRAY2BGR)
+        out.write(new_frame)
         cv2.imshow('frame', new_frame)
 
         # the 'q' button is set as the
@@ -32,6 +40,7 @@ def main():
 
     # After the loop release the cap object
     vid.release()
+    out.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
 
