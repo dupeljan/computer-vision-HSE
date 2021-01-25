8edd472ecb427afdf72643432b338f83e7300106
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-01-25 10:30:55 +0300

    Circle approx with comments

--- 2021/1/1.py
+++ 2021/1/1.py
@@ -13,19 +13,46 @@ def resize(img, scale_percent=30):
 
 def count_balls(image_path):
     # Read image
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(image_path)
+    # Make it b&w
+    b = img[:, :, 2]
+    bw = cv2.merge([b]*3)
+    bw = cv2.cvtColor(bw, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("blue", resize(bw))
+    cv2.waitKey()
     # Make binarization
-    blur = cv2.GaussianBlur(img, (31, 31), 0)
-    threshold, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    thresh = cv2.dilate(thresh, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20)))
+    #blur = cv2.GaussianBlur(img, (31, 31), 0)
+    threshold, thresh = cv2.threshold(bw, 20, 255, cv2.THRESH_BINARY)#+ cv2.THRESH_OTSU)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
+    thresh = cv2.cvtColor(cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR), cv2.COLOR_BGR2GRAY)
+   # cv2.imshow("thresh", resize(thresh))
+   # cv2.waitKey()
+    circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 2, 40,
+                              param1=200, param2=40, minRadius=0, maxRadius=100)
+    assert circles is not None
+    circles = np.uint16(np.around(circles))
+    radius_list = circles[0, :, 2]
+    mean = radius_list.mean()
+    d = ((radius_list - mean)**2).mean()
+    for i in circles[0, :]:
+        # draw the outer circle
+        cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    cv2.imshow(f"Detected circles: count = {len(radius_list)}, mean = {mean}, dispersion = {d}", resize(img))
+    cv2.waitKey()
+    #thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20)))
+    #thresh = cv2.dilate(thresh,kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
     # Find better approx for connected components
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # Copy all cc form original image
     #cv2.drawContours(blur, contours, -1, (0, 255, 0), 3)
-    cv2.imshow("thresh first", cv2.cvtColor(resize(blur), cv2.COLOR_GRAY2BGR))
+    cv2.imshow("thresh first", cv2.cvtColor(resize(thresh), cv2.COLOR_GRAY2BGR))
     cv2.waitKey()
+    '''
     border = 40
-    height, width= img.shape
+    height, width = bw.shape
     for contour in contours[1:]:
         x, y, w, h = cv2.boundingRect(contour)
         # Make it little bit bigger
@@ -40,10 +67,10 @@ def count_balls(image_path):
 
     cv2.imshow("blur after", cv2.cvtColor(resize(thresh), cv2.COLOR_GRAY2BGR))
     cv2.waitKey()
-    cv2.drawContours(img, cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0], -1, (0, 255, 0), 3)
-    cv2.imshow(f"balls count: {-1}", cv2.cvtColor(resize(img), cv2.COLOR_GRAY2BGR))
+    cv2.drawContours(bw, cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0], -1, (0, 255, 0), 3)
+    cv2.imshow(f"balls count: {-1}", cv2.cvtColor(resize(bw), cv2.COLOR_GRAY2BGR))
     cv2.waitKey()
-    '''
+    
     img = cv2.erode(img, np.ones((10, 10), np.int8), iterations=2)
     img = cv2.dilate(img, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)), iterations=3)
     num_labels, labels_image = cv2.connectedComponents(img)
@@ -63,8 +90,8 @@ def count_balls(image_path):
     cv2.waitkey()
     '''
 
-    return num_labels - 1
+    return {"count": len(radius_list), "mean": mean, "dispersion": d}
 
 if __name__ == '__main__':
-    count_balls(IMAGE_PATH)
+    print(count_balls(IMAGE_PATH))
 
